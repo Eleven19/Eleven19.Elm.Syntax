758aa3c39c2b3bdc3c1f9d8ef474aa7486e9561a
DamianReeves <957246+DamianReeves@users.noreply.github.com>
2023-05-01 22:50:36 +0000

    Bump version to 0.0.1-alpha-001
    ## [0.0.1-alpha-001] - 2023-05-01
    
    ### Added
    - Introduce Range and Location types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,13 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.0.1-alpha-001] - 2023-05-01
 
 ### Added
 - Introduce Range and Location types
+[Unreleased]: https://github.com/Eleven19/Eleven19.Elm.Syntax/compare/v0.0.1-alpha-001...HEAD
+[0.0.1-alpha-001]: https://github.com/Eleven19/Eleven19.Elm.Syntax/releases/tag/v0.0.1-alpha-001
--- src/Eleven19.Elm.Syntax/AssemblyInfo.fs
+++ src/Eleven19.Elm.Syntax/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Eleven19.Elm.Syntax")>]
 [<assembly: AssemblyProductAttribute("Eleven19.Elm.Syntax")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-05-01T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","67a2db2e2757f6d721419398ef5c543a66f30157")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Eleven19.Elm.Syntax"
     let [<Literal>] AssemblyProduct = "Eleven19.Elm.Syntax"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyVersion = "0.0.1"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-05-01T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "0.0.1"
+    let [<Literal>] AssemblyInformationalVersion = "0.0.1"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
+    let [<Literal>] AssemblyMetadata_GitHash = "67a2db2e2757f6d721419398ef5c543a66f30157"
